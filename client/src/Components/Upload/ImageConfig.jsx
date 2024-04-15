d0b6bc00a7beff7ea3e7ad5406ecd82215e39334
iksdehh <120662278+iksdehh@users.noreply.github.com>
2024-04-15 13:26:07 +0200

    uploadsection eingebaut

---
+++ client/src/Components/Upload/ImageConfig.jsx
@@ -0,0 +1,12 @@
+import fileCSS from '../../img/file-css-solid-240.png';
+import filePdf from '../../img/file-pdf-solid-240.png';
+import filePng from '../../img/file-png-solid-240.png';
+import fileDefault from "../../img/file-document-solid.png"
+
+
+export const ImageConfig = {
+    default: fileDefault,
+    pdf: filePdf,
+    png: filePng,
+    css: fileCSS
+}
--- client/src/Components/Upload/Upload.jsx
+++ client/src/Components/Upload/Upload.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import "./Upload.css"
+import DropComponents from "./DropComponents"
 
 const Upload = () => {
-  return (
-    <div>Upload</div>
-  )
+
+    const onFileChange = (files) => {
+        console.log(files);
+    }
+
+    return (
+        <div className="upload-main-container">
+            <div className="upload-container">
+                <h2 className="upload-header">
+                    React drop files input
+                </h2>
+                <DropComponents
+                    onFileChange={(files) => onFileChange(files)}
+                />
+            </div>
+        </div>
+    );
 }
 
-export default Upload
+export default Upload      
+
+{/* <div className='absolute z-[3] -left-1/2 top-0 w-[50] h-[50] rounded-full white__gradient'/>
+<div className='absolute z-[0] -left-1/2 bottom-0 w-[50] h-[50] rounded-full pink__gradient'/> */}    
+
+        // <div className="upload-main-container">
+
+        // <div className="upload-container">
+        // <h1 className="upload-header">
