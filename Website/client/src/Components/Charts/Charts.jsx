53400fa0211f5c9319cc9473458d60c1624bb3bd
iksdehh <120662278+iksdehh@users.noreply.github.com>
2024-05-27 14:41:44 +0200

    Anpassung Apriori Chart und implmentierung des Ladesysmbols (Eieruhr)

--- Website/client/src/Components/Charts/Charts.jsx
+++ Website/client/src/Components/Charts/Charts.jsx
@@ -9,14 +9,12 @@ import { RingLoader } from 'react-spinners';
 import { useData } from '../DataContext/DataContext';
 
 const Charts = () => {
-
   const { fetchData, data: apiResponse, loading, error } = useData(); 
-
 
   return (
     <div className='charts bg-[#252525]'>
       <h1 className='partingline'>Lorem Ipsum</h1>
-      <RingLoader speedMultiplier={1} color="var(--logoColor)" className='mr-auto ml-auto' />
+      {loading && <RingLoader speedMultiplier={1} color="var(--logoColor)" className='mr-auto ml-auto' />}
       <hr className='partingline_one' />
 
       <div className='flex min-h-screen flex-col items-center justify-center px-4 md:px-6 xl:px-8 py-33'>
@@ -29,14 +27,13 @@ const Charts = () => {
           </GridItem>
         </div>
         <div className='grid xl:grid-cols-1 lg:grid-cols-1 w-full gap-10 max-w-[1440px] mt-8'>
-            <div className="flex flex-col items-center justify-center pl-2 pb-2 pt-8 border border-slate-900 bg-slate-900/50 rounded-xl h-[600px]">
-               <h3 className="text-2xl font-semibold text-white mb-4">Apriori Chart</h3>
-              <AprioriChart />
-            </div> 
+          <div className="flex flex-col items-center justify-center pl-2 pb-2 pt-8 border border-slate-900 bg-slate-900/50 rounded-xl h-[600px]">
+            <h3 className="text-2xl font-semibold text-white mb-4">Apriori Chart</h3>
+            <AprioriChart />
+          </div> 
         </div>
       </div>
       <h1 className='text-white'>{JSON.stringify(apiResponse)}</h1>
-
     </div>
   );
 }
